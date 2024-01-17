1b8f9fd384878c5c580edc447a037e0577ef4f80
abualhamd <141117351+abualhamd-tot@users.noreply.github.com>
2024-01-17 14:35:31 +0200

    refactored consts into params

--- lib/view/components/order_components/listview_with_header.dart
+++ lib/view/components/order_components/listview_with_header.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tot_atomic_design/tot_atomic_design.dart';
+import 'package:tot_pos/view/components/order_components/order_alert_dialog.dart';
 
 import '../../../core/theme/palette.dart';
+import '../../blocs/order_details/order_details_bloc.dart';
 import '../../blocs/orders/orders_bloc.dart';
 import 'order_card.dart';
 
@@ -68,54 +70,78 @@ class _TOTListViewWithHeaderOrganismState
               itemBuilder: (context, index) => Column(
                 children: [
                   Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: statusesColors[index],
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        width: w * 0.25,
-                        height: h * 0.05,
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                statuses[index].toString(),
-                                style: context.titleMedium,
-                              ),
-                              const SizedBox(width: 10),
-                              Container(
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 12, vertical: 4),
-                                decoration: BoxDecoration(
-                                    color: Palette.primary, // Palette.blue,
-                                    borderRadius: BorderRadius.circular(20)),
-                                child: Text(
-                                  value.orders
-                                      .where((element) =>
-                                          element.status == statuses[index])
-                                      .toList()
-                                      .length
-                                      .toString(),
-                                  style: context.titleMedium.copyWith(
-                                    color: Palette.white,
-                                  ),
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: statusesColors[index],
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      width: w * 0.25,
+                      height: h * 0.05,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              statuses[index].toString(),
+                              style: context.titleMedium,
+                            ),
+                            const SizedBox(width: 10),
+                            Container(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 12, vertical: 4),
+                              decoration: BoxDecoration(
+                                  color: Palette.primary, // Palette.blue,
+                                  borderRadius: BorderRadius.circular(20)),
+                              child: Text(
+                                value.orders
+                                    .where((element) =>
+                                        element.status == statuses[index])
+                                    .toList()
+                                    .length
+                                    .toString(),
+                                style: context.titleMedium.copyWith(
+                                  color: Palette.white,
                                 ),
                               ),
-                            ],
-                          ),
+                            ),
+                          ],
                         ),
-                      )),
-                  TOTOrderCardMolecule(
-                    height: MediaQuery.sizeOf(context).height * 0.81,
-                    orderEntity: value.orders
+                      ),
+                    ),
+                  ),
+                  Builder(builder: (context) {
+                    final orderEntities = value.orders
                         .where((order) =>
                             order.status.toLowerCase() ==
                             statuses[index].toLowerCase())
-                        .toList(),
-                  ),
+                        .toList();
+                    return SizedBox(
+                      height: MediaQuery.sizeOf(context).height * 0.81,
+                      width: MediaQuery.sizeOf(context).width * 0.25,
+                      child: ListView.builder(
+                        itemCount: orderEntities.length,
+                        itemBuilder: (context, index) {
+                          final orderEntity = orderEntities[index];
+                          return TOTOrderCardMolecule(
+                            onTap: (orderEntity) async {
+                              context.read<OrderDetailsBloc>().add(
+                                  OrderDetailsEvent.getOrderbyId(
+                                      orderEntity.id));
+                              await showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return const OrderAlertDialog();
+                                },
+                              );
+                            },
+                            orderEntity: orderEntity,
+                          );
+                        },
+                      ),
+                    );
+                  }),
                 ],
               ),
             ),
