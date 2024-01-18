4a5a4ad179e1af60174f07b143df0c84e303c832
Muhammad Abu-Alhamd <141117351+abualhamd-tot@users.noreply.github.com>
2024-01-18 14:09:08 +0200

    created orders_rail_organism (#56)

--- lib/view/components/order_components/listview_with_header.dart
+++ lib/view/components/order_components/listview_with_header.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:tot_atomic_design/tot_atomic_design.dart';
-import 'package:tot_pos/view/components/order_components/order_alert_dialog.dart';
 import 'package:tot_pos/view/ui_mappers/order_entity_to_pos_record.dart';
 
 import '../../../core/theme/palette.dart';
 import '../../blocs/order_details/order_details_bloc.dart';
 import '../../blocs/orders/orders_bloc.dart';
-import 'order_card.dart';
+import 'order_alert_dialog.dart';
+import 'pos_orders_rail_organism.dart';
 
-class TOTListViewWithHeaderOrganism extends StatefulWidget {
+class TOTListViewWithHeaderOrganism extends StatelessWidget {
   final VoidCallback onPressed;
 
   const TOTListViewWithHeaderOrganism({
@@ -17,14 +16,7 @@ class TOTListViewWithHeaderOrganism extends StatefulWidget {
     required this.onPressed,
   });
 
-  @override
-  State<TOTListViewWithHeaderOrganism> createState() =>
-      _TOTListViewWithHeaderOrganismState();
-}
-
-class _TOTListViewWithHeaderOrganismState
-    extends State<TOTListViewWithHeaderOrganism> {
-  List<String> statuses = [
+  static const List<String> _statuses = [
     "New",
     "Accepted",
     "Prepared",
@@ -33,7 +25,8 @@ class _TOTListViewWithHeaderOrganismState
     "Cancelled",
     "Rejected",
   ];
-  List<Color> statusesColors = [
+
+  static const List<Color> _statusesColors = [
     Palette.yellow,
     Palette.yellow,
     Palette.green,
@@ -42,6 +35,7 @@ class _TOTListViewWithHeaderOrganismState
     Palette.red,
     Palette.red,
   ];
+
   @override
   Widget build(BuildContext context) {
     double w = MediaQuery.of(context).size.width;
@@ -65,87 +59,35 @@ class _TOTListViewWithHeaderOrganismState
           getOrdersSuccess: (value) => SizedBox(
             height: MediaQuery.sizeOf(context).height * 0.94,
             child: ListView.builder(
-              shrinkWrap: true,
-              itemCount: statuses.length,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (context, index) => Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: statusesColors[index],
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      width: w * 0.25,
-                      height: h * 0.05,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              statuses[index].toString(),
-                              style: context.titleMedium,
-                            ),
-                            const SizedBox(width: 10),
-                            Container(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 12, vertical: 4),
-                              decoration: BoxDecoration(
-                                  color: Palette.primary, // Palette.blue,
-                                  borderRadius: BorderRadius.circular(20)),
-                              child: Text(
-                                value.orders
-                                    .where((element) =>
-                                        element.status == statuses[index])
-                                    .toList()
-                                    .length
-                                    .toString(),
-                                style: context.titleMedium.copyWith(
-                                  color: Palette.white,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                  Builder(builder: (context) {
-                    final orderEntities = value.orders
-                        .where((order) =>
-                            order.status.toLowerCase() ==
-                            statuses[index].toLowerCase())
-                        .toList();
-                    return SizedBox(
-                      height: MediaQuery.sizeOf(context).height * 0.81,
-                      width: MediaQuery.sizeOf(context).width * 0.25,
-                      child: ListView.builder(
-                        itemCount: orderEntities.length,
-                        itemBuilder: (context, index) {
-                          final orderEntity = orderEntities[index];
-                          return TOTOrderCardMolecule(
-                            onTap: (orderId) async {
-                              context.read<OrderDetailsBloc>().add(
-                                  OrderDetailsEvent.getOrderbyId(
-                                      orderId));
-                              await showDialog(
-                                context: context,
-                                builder: (BuildContext context) {
-                                  return const OrderAlertDialog();
-                                },
-                              );
-                            },
-                            orderRecord: orderEntity.toPosOrderRecord(),
-                          );
+                shrinkWrap: true,
+                itemCount: _statuses.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  final status = _statuses[index];
+                  final statusColor = _statusesColors[index];
+                  final orderEntities = value.orders
+                      .where((order) =>
+                          order.status.toLowerCase() == status.toLowerCase())
+                      .toList();
+                  return TOTPOSOrdersRailOrganism(
+                    onCardTapped: (orderId) async {
+                      context
+                          .read<OrderDetailsBloc>()
+                          .add(OrderDetailsEvent.getOrderbyId(orderId));
+                      await showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return const OrderAlertDialog();
                         },
-                      ),
-                    );
-                  }),
-                ],
-              ),
-            ),
+                      );
+                    },
+                    posOrderRecords:
+                        orderEntities.map((e) => e.toPosOrderRecord()).toList(),
+                    status: status,
+                    statusColor: statusColor,
+                    countBackgroundColor: Palette.primary,
+                  );
+                }),
           ),
         );
       },
